f938147a9b66f70ece7e3d4e228d7c351949584b
Graphist <graphist233@gmail.com>
2019-08-26 09:55:39 +0430

    fix text
    fix text for send message to any group

--- Wellcomebot.php
+++ Wellcomebot.php
@@ -1,28 +1,24 @@
 <?php
-//use telegram token
-//if u have any question  : graphist233@gmail.com , @fr34k233
-//............................wellcome................................//
+//............................Wellcome................................//
 include("Telegram.php");
-
 date_default_timezone_set("asia/tehran");
-
-// Set the bot TOKEN
-
-$bot_id = 'token';
-
-// Instances the class
-
-$telegram = new Telegram($bot_id);
-$result       = $telegram->getData();
-$text 			  = $telegram->Text();
-$chat_id 		  = $telegram->ChatID();
-$message_id   = $telegram->MessageID();
+//-------------------Set the bot TOKEN------------------
+$bot_id = "TOKEN_HERE";
+//----------Instances the class--------------
+$telegram         = new Telegram ($bot_id);
+$result           = $telegram-> getData ();
+$text 		  = $telegram-> Text ();
+$chat_id 	  = $telegram-> ChatID ();
+$message_id 	  = $telegram-> MessageID ();
+$title            = $telegram-> messageFromGroupTitle();
 $new_chat_members = $result["message"]["new_chat_members"];
-$random_welcome = ["this is default wellcome message"];
-//u can use more than a message here and it will show random
-  if(empty($text) && !empty($new_chat_members)){
-	$rnd_num = mt_rand(1,count($random_welcome)-1);
+//-----------------send content code--------------
+$random_welcome      = ['ورود شما را به' . $title .'تبریک می گویم'];
+if(empty($text) && !empty($new_chat_members)){
+	$rnd_num     = mt_rand(1,count($random_welcome)-1);
 	$welcome_msg = $random_welcome[$rnd_num];
-	$content = array('chat_id' => $chat_id, 'reply_to_message_id' => $message_id, 'text' => $welcome_msg);
-	$telegram->sendMessage($content);
+	$content     = array('chat_id' => $chat_id, 'reply_to_message_id' => $message_id, 'text' => $welcome_msg);
+	$telegram   -> sendMessage($content);
 }
+
+?>
